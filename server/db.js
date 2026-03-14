65ce3b0b08f18c3d58963640b8f8edb4f4517714
Haarjot <harjot23704@gmail.com>
2026-03-14 21:13:50 +0530

    db changes

--- server/db.js
+++ server/db.js
@@ -1,17 +1,19 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const { Pool } = pkg;
 
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL 
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+      }
+);
 
 export default pool;
