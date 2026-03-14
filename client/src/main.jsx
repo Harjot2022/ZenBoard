7d3074725c7f848bdc1a8de411f630354a9ea958
Haarjot <harjot23704@gmail.com>
2026-03-14 22:25:34 +0530

    add vercel routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './index.css'; // Or whatever your main css file is named
 
 createRoot(document.getElementById('root')).render(
   <>
