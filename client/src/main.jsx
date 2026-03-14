be91151ccad12fd0b8ef26302bbacc59a7a7aa36
Haarjot <harjot23704@gmail.com>
2026-03-14 22:46:28 +0530

    Fix Vite CSS import for production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './index.css'; // Or whatever your main css file is named
+import './index.css';
+import './style.css'; 
 
 createRoot(document.getElementById('root')).render(
   <>
